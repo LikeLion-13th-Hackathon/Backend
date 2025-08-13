008f8b5e5e86120ca86af546a9c67e3c8b72012d
ziwonii <nayanako03@gmail.com>
2025-08-14 00:08:36 +0900

    feat: sign-up

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from testapi.views import *
+from accounts.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('testapi.urls')),
+    path('account/', include('accounts.urls')),
 ]
