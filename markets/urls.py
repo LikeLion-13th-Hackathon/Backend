20b38b6930870213f21e2c494f4cf6cbb6a2c816
ziwonii <nayanako03@gmail.com>
2025-08-20 17:09:19 +0900

    feat: List API

--- markets/urls.py
+++ markets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views # 또는 다른 뷰를 import
+from .views import MarketList
 
 urlpatterns = [
     # path('경로/', views.함수명, name='경로이름'),
+    path('list/', MarketList.as_view(), name = 'MarketList')
 ]
