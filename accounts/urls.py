acac1299665fdc0644b118fcdbd38d78f9fa7f6c
wontop02 <wjy0502kr@naver.com>
2025-08-22 14:41:08 +0900

    feat: url change

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("join/", RegisterView.as_view()),
     path("login/", AuthView.as_view()), 
     path("logout/", LogoutView.as_view()), 
-    path("mypage/", UserInfoView.as_view()),
+    path("userinfo/", UserInfoView.as_view()),
     path('reward/', RewardView.as_view())
 ]
